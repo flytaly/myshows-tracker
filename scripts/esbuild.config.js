a401a5d4ffab9dfe7347545165e85f0b42b80504
Vitaly Yerofeyevsky <flytaly@gmail.com>
2023-04-07 02:57:37 +0300

    Update packages

--- scripts/esbuild.config.js
+++ scripts/esbuild.config.js
@@ -1,25 +1,27 @@
-import { build } from 'esbuild';
+import * as esbuild from 'esbuild';
 import { IS_DEV, log, getEnvKeys, TARGET } from './utils.js';
 
 const outdir = './extension/dist';
 
-build({
-    entryPoints: [
-        './src/js/background/background.js', //
-        './src/js/popup/popup.js',
-        './src/js/options/options.js',
-        './src/js/popup-noauth.js',
-    ],
-    bundle: true,
-    outdir,
-    define: getEnvKeys(),
-    watch: IS_DEV,
-    format: 'esm',
-})
-    .then(() => {
-        log(`esbuild`, `Built background and content scripts for ${TARGET}`);
-        if (IS_DEV) {
-            log('esbuild', `watch for changes`);
-        }
-    })
-    .catch(() => process.exit(1));
+async function run() {
+    const context = await esbuild.context({
+        entryPoints: [
+            './src/js/background/background.js', //
+            './src/js/popup/popup.js',
+            './src/js/options/options.js',
+            './src/js/popup-noauth.js',
+        ],
+        bundle: true,
+        outdir,
+        define: getEnvKeys(),
+        format: 'esm',
+    });
+
+    log(`esbuild`, `Built background and content scripts for ${TARGET}`);
+    if (IS_DEV) {
+        log('esbuild', `watch for changes`);
+        await context.watch();
+    }
+}
+
+run();
